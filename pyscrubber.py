bf47a7c55b64b8f2177a9bc9bab9b2953573a152
Robert Hafner <tedivm@tedivm.com>
2019-05-18 14:11:04 -0700

    Add dockerized pyscrubber.py to this file

--- pyscrubber.py
+++ pyscrubber.py
@@ -9,6 +9,7 @@ DOC_DELIMITER = '\n##### DOCUMENT ##############################################
 
 
 class Scrubber():
+    '''This class is a wrapper around the `nlm_scrubber` library.'''
     def __init__(self, working_directory='/tmp/nlm_scrubber'):
         self.working_directory = working_directory
 
@@ -26,18 +27,20 @@ class Scrubber():
             os.makedirs(output_path)
 
 
-    def scrub(self, inputs):
+    def scrub(self, inputs, docker=True):
 
         my_uuid = str(uuid.uuid4())
         base_path = '%s/%s' % (self.working_directory, my_uuid)
         self._setup(base_path)
 
-        self.config_file = '%s/config' % (base_path)
-        with open(self.config_file, 'w') as file:
-            file.write('ROOT1 = %s\n' % (base_path))
-            file.write('ClinicalReports_dir = ROOT1/input\n')
-            file.write('ClinicalReports_files = .*\\.txt\n')
-            file.write('nPHI_outdir = ROOT1/output\n')
+
+        if not docker:
+            self.config_file = '%s/config' % (base_path)
+            with open(self.config_file, 'w') as file:
+                file.write('ROOT1 = %s\n' % (base_path))
+                file.write('ClinicalReports_dir = ROOT1/input\n')
+                file.write('ClinicalReports_files = .*\\.txt\n')
+                file.write('nPHI_outdir = ROOT1/output\n')
 
         for index, input in enumerate(inputs):
             # Write string to disk
@@ -45,7 +48,13 @@ class Scrubber():
                 file.write(input)
 
         # Run scrubber with config
-        result = subprocess.run(['/opt/nlm_scrubber', self.config_file], stdout=subprocess.PIPE)
+        if docker:
+            input_path = '%s/input' % base_path
+            output_path = '%s/output' % base_path
+            run = 'docker run -it --rm -v %s:/tmp/once_off/input -v %s:/tmp/once_off/output --env SCRUBBER_REGEX radaisystems/nlm-scrubber' % (input_path, output_path)
+            result = subprocess.run(run, capture_output=True, shell=True, env={'SCRUBBER_REGEX':'.*\.txt'})
+        else:
+            result = subprocess.run(['/opt/nlm_scrubber', self.config_file], capture_output=True)
 
         outputs = []
         for index, input in enumerate(inputs):
@@ -62,6 +71,10 @@ class Scrubber():
         return outputs
 
 
-if __name__ == "__main__":
+def scrub(inputs):
     scrubber = Scrubber()
-    print(scrubber.scrub(['testing', 'My name is Robert Hafner.', 'This string is also a test. 1/19/1998']))
+    return scrubber.scrub(inputs)
+
+
+if __name__ == "__main__":
+    print(scrub(['testing', 'My name is Robert Hafner.', 'This string is also a test. 1/19/1998']))
